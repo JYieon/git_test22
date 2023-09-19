60833a76b373555cca68c25d3168276d5ccc4f18
Choi Jiyeon <dkcl.8642@gmail.com>
2023-09-20 02:35:48 +0900

    Hello클래스에 messge 변수 추가

--- src/git_test22/Hello.java
+++ src/git_test22/Hello.java
@@ -2,8 +2,9 @@ package git_test22;
 
 public class Hello {
 	
-	public void say() {
-		System.out.println("안녕하세요");
-	}
+	String message = "안녕하세요";
 
+	public void say(){
+		System.out.println(message);
+	}
 }
